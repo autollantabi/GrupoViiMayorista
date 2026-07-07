c04f4359d477c3a168ef8cd7b20363ecb4ec87ba
autollantabi <sfigueroa@maxximundo.com>
2026-07-07 10:56:27 -0500

    refactor: orden de las dependencias de calculateLineTotalWithIVA, handleCheckoutSingleLineInternal, processLineOrder, handleNuveiSuccess, handleNuveiErro yuseNuvei

--- src/api/payments/apiPayments.js
+++ src/api/payments/apiPayments.js
@@ -1,10 +1,22 @@
 import api from "../../constants/api"
 
-export const api_generate_payment_reference = async ({order, user}) => {
+export const api_generate_payment_reference = async ({ order, user }) => {
     try {
         const response = await api.post("payments/generate-paymente-reference", {
             order, user
         });
+        return { success: true, data: response.data.data };
+    } catch (error) {
+        return {
+            success: false,
+            error: error?.response?.data?.message || error.message,
+        };
+    }
+}
+
+export const api_verify_transaction = async (transaction_id) => {
+    try {
+        const response = await api.get(`payments/verify-transaction?transaction_id=${transaction_id}`)
         return { success: true, data: response.data.data };
     } catch (error) {
         return {
